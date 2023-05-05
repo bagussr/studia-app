a2007db5e4698264751e42235d71f6abf435ec0a
bagussr <bagusssyamsu@gmail.com>
2023-05-05 13:06:50 +0700

    login register screen

--- client-studia/src/pages/auth/Login.tsx
+++ client-studia/src/pages/auth/Login.tsx
@@ -9,15 +9,28 @@ import {
   Heading,
   Button,
   Text,
-  Divider,
+  FormErrorMessage,
   Center,
+  Divider,
 } from '@chakra-ui/react';
 import { Link as LinkRouter } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
 import logo from '../../assets/images/logo.png';
+import google from '../../assets/images/google-logo.webp';
+import type { HandleLogin, UserLogin } from '../../types/auth/type';
 
 export const Login: react.FC = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting, touchedFields },
+  } = useForm<UserLogin>();
+
+  const handleLogin: HandleLogin = (data: UserLogin) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Box
@@ -29,8 +42,9 @@ export const Login: react.FC = () => {
         mt={{ base: '0', sm: '32' }}>
         <Image src={logo} alt='logo studia' />
         <Container
+          onSubmit={handleSubmit(handleLogin)}
           as='form'
-          w={{ sm: '1/2', xl: '2/3', base: '3/4' }}
+          w={{ md: '2/3', base: '3/4' }}
           display='flex'
           flexDir='column'
           rowGap={4}>
@@ -46,13 +60,36 @@ export const Login: react.FC = () => {
               Daftar
             </Link>
           </Box>
-          <FormControl isRequired>
-            <Input placeholder='Nomor Ponsel atau Email' />
+          <FormControl isInvalid={errors?.username ? true : false}>
+            <Input
+              placeholder='Username atau Email'
+              {...register('username', {
+                required: 'Username or Email is Required',
+              })}
+            />
+            <FormErrorMessage>{errors?.username?.message}</FormErrorMessage>
           </FormControl>
-          <FormControl isRequired>
-            <Input placeholder='Kata Sandi' type='password' />
+          <FormControl isInvalid={errors?.password ? true : false}>
+            <Input
+              placeholder='Kata Sandi'
+              type='password'
+              {...register('password', {
+                required: 'Password is Required',
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters ',
+                },
+              })}
+            />
+            <FormErrorMessage>{errors?.password?.message}</FormErrorMessage>
           </FormControl>
-          <Button colorScheme='gray'>Masuk</Button>
+          <Button
+            colorScheme='gray'
+            isLoading={isSubmitting}
+            type='submit'
+            fontSize={{ base: 'sm', lg: 'md' }}>
+            Masuk
+          </Button>
           <Text alignSelf='start' mt='5'>
             Lupa kata sandi? <Link as={LinkRouter}>Klik disini!</Link>
           </Text>
@@ -70,6 +107,18 @@ export const Login: react.FC = () => {
               <Divider />
             </Center>
           </Box>
+          <Button
+            mt='5'
+            display='flex'
+            variant='google'
+            fontSize={{ base: 'sm', lg: 'md' }}>
+            <Image
+              src={google}
+              height={{ lg: '6', base: '4' }}
+              mr={{ lg: '4', base: '2' }}
+            />{' '}
+            Masuk dengan Google
+          </Button>
         </Container>
       </Box>
     </>
